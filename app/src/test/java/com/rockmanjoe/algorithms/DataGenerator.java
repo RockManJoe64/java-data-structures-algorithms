f1899e592694da6002b4c2792435cca76d52d146
Jose Chavez <jose.e.chavez@gmail.com>
2023-09-06 23:13:12 -0700

    Implemented quicksort algorithm

---
+++ app/src/test/java/com/rockmanjoe/algorithms/DataGenerator.java
@@ -0,0 +1,36 @@
+package com.rockmanjoe.algorithms;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public final class DataGenerator {
+    public static int[] generateSortedArray(int size) {
+        var array = new int[size];
+        for (var i = 0; i < size; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    public static int[] generateRandomArray(int size) {
+        var array = new int[size];
+        for (var i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * size);
+        }
+        return array;
+    }
+
+    public static int[] generateShuffledArray(int size) {
+        var shuffledList = Arrays.stream((DataGenerator.generateSortedArray(100)))
+                .boxed()
+                .toList();
+        shuffledList = new LinkedList<>(shuffledList);
+
+        Collections.shuffle(shuffledList);
+
+        return shuffledList.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
